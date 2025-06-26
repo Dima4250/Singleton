630be533a547338e9502262afc88b357118d5408
Dima4250 <dmitriy.vorobiev2016@yandex.ru>
2025-06-26 17:46:33 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Singleton
+{
+    // Статическое поле для хранения единственного экземпляра
+    private static Singleton _instance;
+
+    // Приватный конструктор (запрет создания через new)
+    private Singleton() { }
+
+    // Публичное свойство для доступа к экземпляру
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+
+    // Какой-то метод класса для проверки жизнеспособности программы
+    public void AGA()
+    {
+        Console.WriteLine("googoogaga");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Singleton singleton = Singleton.Instance;
+        singleton.AGA();
+    }
+}
